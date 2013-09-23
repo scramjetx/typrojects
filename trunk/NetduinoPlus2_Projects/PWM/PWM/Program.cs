508900f8a1f70fb9a0864f8c1f55e70980540197
scramjetx@gmail.com <scramjetx@gmail.com@b3c99014-6616-d279-393f-2dbfcd3d0caa>
2013-09-23 23:56:00 +0000

    neduino stuff

---
+++ trunk/NetduinoPlus2_Projects/PWM/PWM/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace PWMtest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            PWM led1 = new PWM(PWMChannels.PWM_ONBOARD_LED, 100, .5, false); // Set later
+            //PWM led2 = new PWM(PWMChannels.PWM_PIN_D5, 50, .5, false); //50% brightness
+            PWM led2 = new PWM(PWMChannels.PWM_PIN_D5, 20000, 1500, PWM.ScaleFactor.Microseconds, false);
+
+            led1.Start();
+            led2.Start();
+
+            Thread.Sleep(1000);
+
+            while (true)
+            {
+                for (double x = 1000; x < 2000; x = x + 20)
+                {
+                    led2.Duration = (uint)x;
+                    Thread.Sleep(100);
+                }
+
+            }
+
+
+        }//end main()
+
+    }
+}
