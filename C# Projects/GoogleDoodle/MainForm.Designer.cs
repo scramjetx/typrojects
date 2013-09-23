90f92fce72994484c724d1eb0289ced5888ebfae
scramjetx@gmail.com <scramjetx@gmail.com@b3c99014-6616-d279-393f-2dbfcd3d0caa>
2013-09-23 22:53:54 +0000

    google doodle did some olympics ones that I tried to game with an app in c#. Pretty fun to play around with see if bball or hurdles could be gamed.  it worked partially

---
+++ C# Projects/GoogleDoodle/MainForm.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Hurdles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Left/Right Macro";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(145, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Basketball Macro";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 400;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 300;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ShotBuffer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Line1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Line2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Line3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(168, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Line4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(231, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(231, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(231, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(231, 135);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(231, 161);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 17;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(231, 187);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(231, 213);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(168, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "ShotBuffer";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(168, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "ShotBuffer";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(168, 194);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "ShotBuffer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 331);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ C# Projects/GoogleDoodle/MainForm.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+using Utilities;
+using System.Runtime.InteropServices;       //import dll
+using System.Threading;
+
+namespace Hurdles
+{
+
+    public partial class Form1 : Form
+    {
+        
+
+        const int VK_SPACE = 0x20;
+        const uint KEYEVENTF_KEYUP = 0x2;
+
+        globalKeyboardHook Skeypress = new globalKeyboardHook();
+        globalKeyboardHook Spacekeypress = new globalKeyboardHook();
+        
+
+        int nKeyCount = 1;
+        int spacekeyCount = 0;
+
+        bool spacebarState = false;     //true is pressed and holding, false is not pressed
+        int countSpacebar = 0;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(
+        byte bVk,
+        byte bScan,
+        uint dwFlags,
+        uint dwExtraInfo
+        );
+
+        double start;
+        double span;
+
+        bool startProgramFlag = true;
+        bool spaceKeyUpFlag = true;
+        bool spaceKeyDownFlag = false;
+
+        //init in text box too
+        int SHOTBUFFER1 = 800;
+        int TIMING1 = 300;
+        int SHOTBUFFER2 = 800;
+        int TIMING2 = 475;
+        int SHOTBUFFER3 = 800;
+        int TIMING3 = 650;
+        int SHOTBUFFER4 = 800;
+        int TIMING4 = 825;
+
+        int TIMING5 = 925;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Skeypress.HookedKeys.Add(Keys.S);
+            Skeypress.KeyDown += new KeyEventHandler(S_KeyDown);
+            Skeypress.KeyUp += new KeyEventHandler(S_KeyUp);
+
+            Spacekeypress.HookedKeys.Add(Keys.Space);
+            Spacekeypress.KeyDown += new KeyEventHandler(Space_KeyDown);
+            Spacekeypress.KeyUp += new KeyEventHandler(Space_KeyUp);
+
+            textBox1.Text = SHOTBUFFER1.ToString();
+            textBox2.Text = TIMING1.ToString();
+            textBox3.Text = SHOTBUFFER2.ToString();
+            textBox4.Text = TIMING2.ToString();
+            textBox5.Text = SHOTBUFFER3.ToString();
+            textBox6.Text = TIMING3.ToString();
+            textBox7.Text = SHOTBUFFER4.ToString();
+            textBox8.Text = TIMING4.ToString();
+
+        }
+
+        void S_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            //lstLog.Items.Add("Down\t" + e.KeyCode.ToString());
+            e.Handled = true;
+        }
+
+        void S_KeyUp(object sender, KeyEventArgs e)
+        {
+            for(int i = 0; i<20; i++)
+            Console.WriteLine();
+
+            SHOTBUFFER1 = Int16.Parse(textBox1.Text.ToString());
+            TIMING1 = Int16.Parse(textBox2.Text.ToString());
+            SHOTBUFFER2 = Int16.Parse(textBox3.Text.ToString());
+            TIMING2 = Int16.Parse(textBox4.Text.ToString());
+            SHOTBUFFER3 = Int16.Parse(textBox5.Text.ToString());
+            TIMING3 = Int16.Parse(textBox6.Text.ToString());
+            SHOTBUFFER4 = Int16.Parse(textBox7.Text.ToString());
+            TIMING4 = Int16.Parse(textBox8.Text.ToString());
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING1);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER1);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING1);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER1);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING1);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER1);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING1);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER1);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING1);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep(700, 50);
+            Thread.Sleep(SHOTBUFFER1);
+            
+            //**line 2
+            Console.WriteLine("Line2");
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING2);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER2);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING2);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER2);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING2);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER2);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING2);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep(700, 50);
+            Thread.Sleep(SHOTBUFFER2);
+
+            //**line 3
+            Console.WriteLine("Line3");
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING3);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER3);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING3);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER3);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING3);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER3);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING3);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep(700, 50);
+            Thread.Sleep(SHOTBUFFER3);
+
+            //**line4
+            Console.WriteLine("Line4");
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING4);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER4);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING4);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER4);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING4);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER4);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING4);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            //Console.Beep();
+            Thread.Sleep(SHOTBUFFER4);
+
+            keybd_event((byte)VK_SPACE, 0, 0, 0);
+            Thread.Sleep(TIMING5);
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+            Console.WriteLine("LAST SHOT");
+
+            //lstLog.Items.Add("Up\t" + e.KeyCode.ToString());
+            e.Handled = true;
+        }
+
+        void Space_KeyDown(object sender, KeyEventArgs e)
+        {
+            //add flag here cause keydown fires many times while holding
+            //if (spaceKeyUpFlag)
+            //{
+            //    start = DateTime.Now.TimeOfDay.TotalMilliseconds;
+            //    spaceKeyUpFlag = false;
+            //    spaceKeyDownFlag = true;
+            //}
+
+            //if (startProgramFlag)
+            //{
+            //    timer4.Interval = 400;
+            //    timer4.Enabled = true;
+            //    startProgramFlag = false;
+            //}
+
+            
+            
+            //Console.WriteLine("keydown");
+        }
+
+        void Space_KeyUp(object sender, KeyEventArgs e)
+        {
+            //if (spaceKeyDownFlag)
+            //{
+            //    span = DateTime.Now.TimeOfDay.TotalMilliseconds - start;
+            //    Console.WriteLine("key press time = " + span.ToString("000.00") + "  COUNT: " + spacekeyCount.ToString());
+            //    span = 0;
+            //    spaceKeyUpFlag = true;
+                
+
+            //    //int temp = spacekeyCount;
+            //    //if (temp == 6 || temp == 10 || temp == 15)
+            //     //   ChangeSpacebarInterval();
+            //    if (spacekeyCount == 3)
+            //    {
+            //        timer4.Stop();
+            //        timer4.Interval = 550;
+            //        timer4.Start();
+            //        Console.WriteLine("Changed Interval: " + timer4.Interval.ToString());
+            //        Console.Beep();
+            //    }
+            //    else if (spacekeyCount == 13)
+            //    {
+            //        timer4.Stop();
+            //        timer4.Interval = 650;
+            //        timer4.Start();
+            //        Console.WriteLine("Changed Interval: " + timer4.Interval.ToString());
+            //        Console.Beep();
+            //    }
+            //    else if (spacekeyCount == 22)
+            //    {
+            //        timer4.Stop();
+            //        timer4.Interval = 700;
+            //        timer4.Start();
+            //        Console.WriteLine("Changed Interval: " + timer4.Interval.ToString());
+            //        Console.Beep();
+            //    }
+            //    else if (spacekeyCount == 28)
+            //    {
+            //        timer4.Interval = 850;
+            //        Console.WriteLine("Changed Interval: " + timer4.Interval.ToString());
+            //        Console.Beep();
+            //    }
+
+            //    spacekeyCount++;
+            //}
+        }
+       
+        private void ChangeSpacebarInterval()
+        {
+            switch (spacekeyCount)
+            {
+                case 5:
+                    timer4.Interval = 750;
+                    break;
+                case 10:
+                    timer4.Interval = 850;
+                    break;
+                case 15:
+                    timer4.Interval = 1000;
+                    break;
+            }
+            Console.WriteLine("Interval Changed to: " + timer4.Interval.ToString());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{LEFT}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+            timer2.Enabled = true;
+            timer2.Interval = 2;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{RIGHT}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            nKeyCount = 1;
+            spacebarState = false;
+            countSpacebar = 0;
+            spaceKeyUpFlag = true;
+            spacekeyCount = 0;
+            startProgramFlag = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer4.Interval = 300;
+            timer4.Enabled = true;
+            
+        
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            //SendKeys.Send(" ");
+
+            if (spacebarState == false)
+            {
+                keybd_event((byte)VK_SPACE, 0, 0, 0);
+                spacebarState = true;
+            }
+            else
+            {
+                keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+                keybd_event((byte)VK_SPACE, 0, 0, 0);
+                spacebarState = false;
+                countSpacebar++;
+
+                Console.WriteLine("SPACEBAR : " + DateTime.Now.Millisecond.ToString());
+                
+                if (countSpacebar % 5 == 0)
+                {
+                    nKeyCount++;
+                    ChangeSpacebarInterval();
+                }
+            }
+
+            
+            
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            keybd_event((byte)VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
+
+        }
+
+ 
+        
+    }
+}
