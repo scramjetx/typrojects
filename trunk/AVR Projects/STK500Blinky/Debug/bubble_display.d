15feb93c0e1e4d12ce09b2f44e32b2898a493db4
scramjetx@gmail.com <scramjetx@gmail.com@b3c99014-6616-d279-393f-2dbfcd3d0caa>
2014-05-17 04:52:57 +0000

    updated bubble

--- trunk/AVR Projects/STK500Blinky/Debug/bubble_display.d
+++ trunk/AVR Projects/STK500Blinky/Debug/bubble_display.d
@@ -1,4 +1,4 @@
 bubble_display.d bubble_display.o: ../bubble_display.c \
-  ../Includes/bubble_display.h
+  ../includes/bubble_display.h
 
-../Includes/bubble_display.h:
+../includes/bubble_display.h:
