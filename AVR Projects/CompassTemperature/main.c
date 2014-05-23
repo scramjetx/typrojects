995bd2f865ef4192ec6cd83985d63e616f520032
scramjetx@gmail.com <scramjetx@gmail.com@b3c99014-6616-d279-393f-2dbfcd3d0caa>
2014-05-23 01:00:32 +0000

    working declaration of pins so can make bubble_display.c agnostic from main.c

--- AVR Projects/CompassTemperature/main.c
+++ AVR Projects/CompassTemperature/main.c
@@ -12,59 +12,74 @@
 #define LOOP_RATE 100		//how fast to run the loop
 #define TICKS_PER_HZ 500	//how many ticks elapse per hz to get the loop rate
 
+#define SEG_A PD2
+
 int main(void)
 {
+	//Segment Anodes
+	DDRA = (1<<PA0)|(1<<PA1);
+	DDRD = (1<<PD2)|(1<<PD3)|(1<<PD4)|(1<<PD5)|(1<<PD6);
+
+	//Segment Cathodes
+	DDRB = (1<<PB1)|(1<<PB2)|(1<<PB3)|(1<<PB4); 	//Cathodes set to output
+	PORTB = (0<<PB1)|(0<<PB2)|(0<<PB3)|(0<<PB4);	//pin set low.
 
 	uint8_t STATE = 1;
 	int64_t timer = 1;  //placeholder for timer ticks to know when to start main state machine and clock how fast it loops
 
 	//timer must be incremented by an interrupt or equivalent...then reset when bottom of if statement
 
-
-	if(timer == TICKS_PER_HZ*LOOP_RATE)
+	while(1)
 	{
 
 
-		//**********************************************
-		//State 1:
-		// Take measurement of compass or temperature depending on which one is selected
-		//**********************************************
-		if(STATE == 1 )
+		if(timer == TICKS_PER_HZ*LOOP_RATE)
 		{
 
 
-			STATE = 2;  //transition to next state
-		}
+			//**********************************************
+			//State 1:
+			// Take measurement of compass or temperature depending on which one is selected
+			//**********************************************
+			if(STATE == 1 )
+			{
 
 
-		//**********************************************
-		//State 2:
-		//Parse reading into distinct digits for display
-		//**********************************************
-		if(STATE == 2)
-		{
+				STATE = 2;  //transition to next state
+			}
 
 
-			STATE = 3;	//transition to next state
-		}
+			//**********************************************
+			//State 2:
+			//Parse reading into distinct digits for display
+			//**********************************************
+			if(STATE == 2)
+			{
 
 
-		//**********************************************
-		//State 3:
-		//display data
-		//**********************************************
-		if(STATE == 3)
-		{
+				STATE = 3;	//transition to next state
+			}
 
 
-			STATE = 1; 	//return to first state
-		}
+			//**********************************************
+			//State 3:
+			//display data
+			//**********************************************
+			if(STATE == 3)
+			{
+
+
+				STATE = 1; 	//return to first state
+			}
+
+			timer = 0;
+		} //end if
 
 
 
+		timer++;
 
-
-    }
+    } //end while(1)
 
 
 
