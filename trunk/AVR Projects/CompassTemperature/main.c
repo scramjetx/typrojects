5733fd2be78dd50a768bbd5fd422f75cfe6534f1
scramjetx@gmail.com <scramjetx@gmail.com@b3c99014-6616-d279-393f-2dbfcd3d0caa>
2014-06-07 01:58:34 +0000

    added build temp array stuff not really working need a way to display ints to serial port

--- trunk/AVR Projects/CompassTemperature/main.c
+++ trunk/AVR Projects/CompassTemperature/main.c
@@ -5,6 +5,10 @@
 //What's Next?
 //then can change test temp to larger number so it prints 003F or 072F for the temp...or maybe no leading 0's
 //figure out how to do display loop so it samples at some slow rate 5hz...then displays the rest of the time at some rate..if want to adjust brightness
+//figure out why merge temp display isn't displaying what I think it should. Passing arrays to functions is killing me.  Data looks like it's static.
+
+
+
 
 //Optimizations on.  Properties->Build->Settings->Optimizations
 
@@ -21,13 +25,16 @@
 
 //Main variables
 char tempDegArray [] = "999";  //hold the char string of temperature reading for display
-int8_t degF = 123;			   //hold temp reading in deg F
+int8_t degReading = 123;			   //hold temp reading in deg F
+uint8_t numDigits = 0;		   //store the number of digits the temp reading has
+char displayTempUnits = 'F';			//what units to display temp
 
 char displayArray [] = "999F"; //holds the string to be displayed
 
 //Function Prototypes
 void parseTempReading(char *, int8_t);
-
+uint8_t findNumDigits(uint8_t);
+char * buildTempDisplayArray(char [], uint8_t, char);
 
 int main(void)
 {
@@ -51,6 +58,7 @@ int main(void)
 
 
 	int8_t testTemp = 0;
+
 
 	while(1)
 	{
@@ -79,10 +87,39 @@ int main(void)
 			if(STATE == 2)
 			{
 
+				parseTempReading(tempDegArray, degReading);
+				numDigits = findNumDigits(degReading);
 
+				//not working
+				USART_SendChar(numDigits);
+				USART_SendBlankline();
 
-				parseTempReading(tempDegArray, degF);
+				//Celsius or Farenheit?
+				displayTempUnits = 'F';
 
+				//Build temperature array to be displayed
+				if(numDigits == 1)
+				{
+					displayArray[2] = tempDegArray[0];
+				}
+				else if(numDigits == 2)
+				{
+					displayArray[1] = tempDegArray[0];
+					displayArray[2] = tempDegArray[1];
+				}
+				else if(numDigits == 2)
+				{
+					displayArray[0] = tempDegArray[0];
+					displayArray[1] = tempDegArray[1];
+					displayArray[2] = tempDegArray[2];
+				}
+
+				displayArray[3] = displayTempUnits;
+
+				USART_SendString(displayArray);
+				USART_SendBlankline();
+
+				//test code
 				testTemp++;
 				if(testTemp>9)
 					testTemp = 0;
@@ -103,7 +140,7 @@ int main(void)
 				//of form '_12F' or '__5F'
 
 
-				charArrayDisplay(&displayArray);
+				charArrayDisplay(displayArray);
 
 				STATE = 1; 	//return to first state
 			}
@@ -126,5 +163,17 @@ void parseTempReading(char * c, int8_t i)
 	//sprintf takes alot of memory to convert a int to a string/char array
 	//could possibly do a modulus operation by 10 to extract digits to save memory
 	sprintf(c, "%d", i);
+
+}
+
+uint8_t findNumDigits(uint8_t num)
+{
+
+	if ( num < 10 )
+		return 1;
+    if ( num < 100 )
+	    return 2;
+    if ( num < 1000 )
+		return 3;
 
 }
