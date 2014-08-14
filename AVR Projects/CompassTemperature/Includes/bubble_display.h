ed38d368eabd4bca42051bd11b404cbb32e8c10f
scramjetx@gmail.com <scramjetx@gmail.com@b3c99014-6616-d279-393f-2dbfcd3d0caa>
2014-08-14 01:29:46 +0000

    it's now converting from C to F and F to C it's displaying negative temps correctly

--- AVR Projects/CompassTemperature/Includes/bubble_display.h
+++ AVR Projects/CompassTemperature/Includes/bubble_display.h
@@ -64,7 +64,7 @@
 //uint8_t displayDigit1pin, displayDigit2pin, displayDigit3pin, displayDigit4pin;
 
 //segment bit order: A, B, C, D, E, F, G, DP
-uint8_t digits [18] = {
+uint8_t digits [19] = {
 		0b11111100, // 0
 		0b01100000, // 1
 		0b11011010, // 2
@@ -101,26 +101,27 @@ uint8_t digits [18] = {
 		//0b0000000, // 88 'X'  NO DISPLAY
 		//0b0111011, // 89 'Y'
 		//0b0000000, // 90 'Z'  NO DISPLAY
-		0b00010000, // _
-		0b00000000  // \0 or NULL
+		0b00010000, // 95 '_'
+		0b00000000,  // \0 or NULL
+		0b00000010	// 45 '-'
 };
 
-uint8_t zero = 0b1111110; // 0
-uint8_t one = 0b0110000; // 1
-uint8_t two = 0b1101101; // 2
-uint8_t three = 0b1111001; // 3
-uint8_t four = 0b0110011; // 4
-uint8_t five = 0b1011011; // 5
-uint8_t six = 0b1011111; // 6
-uint8_t seven = 0b1110000; // 7
-uint8_t eight = 0b1111111; // 8
-uint8_t nine = 0b1111011; // 9
+//uint8_t zero = 0b1111110; // 0
+//uint8_t one = 0b0110000; // 1
+//uint8_t two = 0b1101101; // 2
+//uint8_t three = 0b1111001; // 3
+//uint8_t four = 0b0110011; // 4
+//uint8_t five = 0b1011011; // 5
+//uint8_t six = 0b1011111; // 6
+//uint8_t seven = 0b1110000; // 7
+//uint8_t eight = 0b1111111; // 8
+//uint8_t nine = 0b1111011; // 9
 //0b1110111, // 65 'A'
 //0b0011111, // 66 'B'
-uint8_t c = 0b1001110; // 12  "C"
+//uint8_t c = 0b1001110; // 12  "C"
 //0b0111101, // 68 'D'
-uint8_t e = 0b1001111; // 69 'E'
-uint8_t f = 0b1000111; // 15  "F"
+//uint8_t e = 0b1001111; // 69 'E'
+//uint8_t f = 0b1000111; // 15  "F"
 //0b1011110, // 71 'G'
 //0b0110111, // 72 'H'
 //0b0110000, // 73 'I'
@@ -128,16 +129,16 @@ uint8_t f = 0b1000111; // 15  "F"
 //0b0000000, // 75 'K'  NO DISPLAY
 //0b0001110, // 76 'L'
 //0b0000000, // 77 'M'  NO DISPLAY
-uint8_t n = 0b0010101; // 78 'N'
+//uint8_t n = 0b0010101; // 78 'N'
 //0b1111110, // 79 'O'
 //0b1100111, // 80 'P'
 //0b1110011, // 81 'Q'
 //0b0000101, // 82 'R'
-uint8_t s = 0b1011011; // 83 'S'
+//uint8_t s = 0b1011011; // 83 'S'
 //0b0001111, // 84 'T'
 //0b0111110, // 85 'U'
 //0b0000000, // 86 'V'  NO DISPLAY
-uint8_t w = 0b0111111; // W
+//uint8_t w = 0b0111111; // W
 //0b0000000, // 88 'X'  NO DISPLAY
 //0b0111011, // 89 'Y'
 //0b0000000, // 90 'Z'  NO DISPLAY
